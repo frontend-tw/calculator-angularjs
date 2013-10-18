4ea0e62cb820be78e4fb516782efa835130731af
Jace Ju <jaceju@kkbox.com>
2013-10-18 18:59:21 +0800

    Add limit of digits to display.

--- js/calculator.js
+++ js/calculator.js
@@ -6,11 +6,17 @@ controller('calculator', function (
     $scope
 ) {
     var self = this;
+
+    self.maxDigits = 8;
+
     $scope.screen = '0';
     self.pressNum = function (num) {
         if ($scope.screen === '0') {
             $scope.screen = '';    
         }
-        $scope.screen += num;
+
+        if ($scope.screen.length < 12) {
+            $scope.screen += num;
+        }
     }
 });
