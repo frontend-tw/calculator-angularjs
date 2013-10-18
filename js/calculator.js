b2e87d6a17a2d8f4f7a42e595e440df744d3289b
Jace Ju <jaceju@kkbox.com>
2013-10-18 18:18:07 +0800

    AngularJS combined.

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,13 @@
 'use strict';
 
-angular.module('Calculator', [
-    'ui.router'
-]).
+angular.module('Calculator2', []).
+
+controller('calculator', function (
+    $scope
+) {
+    $scope.display = '0';
+
+    this.pressNum = function (num) {
+        $scope.display = num;
+    }
+});
